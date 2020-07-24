8d37923e1070dd5c2162ddc6c7fb95d2d36a666b
pradnyanivangune <68327275+pradnyanivangune@users.noreply.github.com>
2020-07-24 22:17:14 +0530

    Add files via upload

---
+++ LogicBuilding/LogicBuilding20July2020/Program12.c
@@ -0,0 +1,37 @@
+//FOR LOOP BASIC #12
+
+#include<stdio.h>
+
+void main(void){
+
+	for(int i=0,j=0 ; i<=10 ; i++, j--){
+			
+		printf("%d %d\n",i,j);
+					
+		
+	}
+
+}
+
+
+/*
+
+Output-
+
+pradnya@pradnya-Latitude-3480:~/Desktop/LogicBuilding$ gedit Program12.c
+pradnya@pradnya-Latitude-3480:~/Desktop/LogicBuilding$ cc Program12.c
+pradnya@pradnya-Latitude-3480:~/Desktop/LogicBuilding$ ./a.out
+0 0
+1 -1
+2 -2
+3 -3
+4 -4
+5 -5
+6 -6
+7 -7
+8 -8
+9 -9
+10 -10
+
+
+*/

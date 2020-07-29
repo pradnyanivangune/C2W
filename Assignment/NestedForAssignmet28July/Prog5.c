1db9a8c55a394de1d3c75af448489691411ddb37
pradnyanivangune <68327275+pradnyanivangune@users.noreply.github.com>
2020-07-29 21:57:58 +0530

    Update Prog5.c

--- Assignment/NestedForAssignmet28July/Prog5.c
+++ Assignment/NestedForAssignmet28July/Prog5.c
@@ -8,4 +8,50 @@ E	a 	D 	b
 
 */
 
+#include<stdio.h>
+void main(){
+
+	int n,k;
+	printf("Enter no. of rows:\n");
+	scanf("%d",&n);
+
+	char ch1 = 'E' ;
+	char ch2 = 'a' ;
+	for(int i = 1 ; i <= n ; i++){
+
+		for(int k = 1 ; k < i  ; k++){
+			printf("\t ");
+			
+		}
+
+		for(int j = i ; j <= n ; j++){
+			if(j%2 != 0){
+				printf("%c\t ",ch1);
+				ch1--;
+			}else{
+				printf("%c\t ",ch2);
+				ch2++;
+			}
+			
+		}
+		
+		printf("\n");
+	}
+}
+
+/*
+
+pradnya@pradnya-Latitude-3480:~/Desktop/Assignment/NestedForAssignmet28July$ gedit Prog5.c
+pradnya@pradnya-Latitude-3480:~/Desktop/Assignment/NestedForAssignmet28July$ cc Prog5.c
+pradnya@pradnya-Latitude-3480:~/Desktop/Assignment/NestedForAssignmet28July$ ./a.out
+Enter no. of rows:
+4
+E	 a	 D	 b	 
+	 c	 C	 d	 
+	 	 B	 e	 
+	 	 	 f
+
+*/
+
+
 
